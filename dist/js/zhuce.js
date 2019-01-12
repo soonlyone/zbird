ec4f29df5e20c233b81e010a01c02d3f0dcf39f8
bw <1456801822@qq.com>
2019-01-12 20:59:57 +0800

    zhuce delv create

---
+++ dist/js/zhuce.js
@@ -0,0 +1,67 @@
+$(function(){
+	var a,b,c,d,flag;
+	flag=0;
+	$.ajax({
+		type:"get",
+		url:"img/user.json",
+		async:true,
+		success:foo
+	});
+	var user=[];
+	function foo(data){
+		for(var i in data){
+			user.push(data[i]);
+		}
+	}
+	$("#phonenum").change(function(){
+		var regnum=/^1(3|5|7|8)\d{9}$/;
+		console.log(regnum.test($(this).val()));
+		if(!$(this).val()){
+			$("#tishi1").html("手机号不能为空").show();
+		}else if(!regnum.test($(this).val())){
+			$("#tishi1").html("手机号格式不对").show();
+		}else{
+			for(var i=0;i<user.length;i++){
+				
+			}
+			$("#tishi1").hide();
+			a=1;
+		}
+	});
+	
+	$("#mimi").change(function(){
+		if(!$(this).val()){
+			$("#tishi2").html("验证码不能为空").show();
+		}else if($(this).val().length!=6){
+			$("#tishi2").html("验证码长度6位").show();
+		}else{
+			b=1
+		}
+	})	
+	$("#pswd").change(function(){
+		var reg=/^\w{6,20}$/;
+		if(!$(this).val()){
+			$("#tishi3").html("密码不能为空").show();
+		}else if(!reg.test($(this).val())){
+			$("#tishi3").html("格式不对").show();
+		}else{
+			c=1;
+		}
+	})
+	$("#pwd").change(function(){		
+		if(!$(this).val()){
+			$("#tishi4").html("验证码不能为空").show();
+		}else if($(this).val()!=$("#pswd").val()){
+			$("#tishi4").html("两次密码不一致").show();
+		}else{
+			d=1;
+		}
+	})
+	$("#joinit").click(function(){
+		flag=a+b+c+d;
+		if(flag!=4){
+			alert("请输入正确信息");
+		}
+	})
+	
+})
